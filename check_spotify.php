ccb0ef03aa419a71886715d8847eece148dc13ac
Idhan Haidar Kurniawan <144966714+DhanHaidar@users.noreply.github.com>
2024-12-20 22:33:34 +0700

    Delete check_spotify.php

--- check_spotify.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (isset($_SESSION['spotify_access_token'])) {
-    echo json_encode(["connected" => true]);
-} else {
-    echo json_encode(["connected" => false]);
-}
-?>
